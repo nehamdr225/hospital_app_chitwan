98ead97336a0f9d6ab9b99d2c961aebf8bc70856
codedump00 <blazinasis@gmail.com>
2020-06-28 14:50:10 +0545

    changed appointment tabs functionality

--- lib/UI/DoctorsModule/AppointmentTabs/DCompleted.dart
+++ lib/UI/DoctorsModule/AppointmentTabs/DCompleted.dart
@@ -1,24 +1,21 @@
 import 'package:chitwan_hospital/UI/DoctorsModule/AppointmentTabs/DoctorListCard.dart';
+import 'package:chitwan_hospital/UI/DoctorsModule/PatientListCard.dart';
 import 'package:chitwan_hospital/UI/core/const.dart';
+import 'package:chitwan_hospital/state/doctor.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DCompleted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
+    final appointments = Provider.of<DoctorDataStore>(context)
+        .appointments
+        .where((element) => element['status'] == 'completed')
+        .toList();
     return ListView.builder(
-          itemCount: NearYou.length,
-          itemBuilder: (BuildContext context, int index) {
-            return DoctorListCard(
-              name: NearYou[index]['name'],
-              caption: NearYou[index]['cap'],
-              image: NearYou[index]['src'],
-              phone: NearYou[index]['phone'],
-              status: NearYou[index]['status'],
-              date: NearYou[index]['date'],
-              time:NearYou[index]['time'],
-            );
-          },
-    );
+        itemCount: appointments.length,
+        itemBuilder: (BuildContext context, int index) {
+          return PatientListCard(id: appointments[index]['id']);
+        });
   }
 }
--- lib/UI/DoctorsModule/AppointmentTabs/DRejected.dart
+++ lib/UI/DoctorsModule/AppointmentTabs/DRejected.dart
@@ -1,24 +1,19 @@
-import 'package:chitwan_hospital/UI/DoctorsModule/AppointmentTabs/DoctorListCard.dart';
-import 'package:chitwan_hospital/UI/core/const.dart';
+import 'package:chitwan_hospital/UI/DoctorsModule/PatientListCard.dart';
+import 'package:chitwan_hospital/state/doctor.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DRejected extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   // final theme = Theme.of(context);
+    final appointments = Provider.of<DoctorDataStore>(context)
+        .appointments
+        .where((element) => element['status'] == 'rejected')
+        .toList();
     return ListView.builder(
-          itemCount: PrescriptionData.length,
-          itemBuilder: (BuildContext context, int index) {
-            return DoctorListCard(
-              name: NearYou[index]['name'],
-              caption: NearYou[index]['cap'],
-              image: NearYou[index]['src'],
-              phone: NearYou[index]['phone'],
-              status: 'Rejected',
-              date: NearYou[index]['date'],
-              time:NearYou[index]['time'],
-            );
-          }
-    );
+        itemCount: appointments.length,
+        itemBuilder: (BuildContext context, int index) {
+          return PatientListCard(id: appointments[index]['id']);
+        });
   }
 }
